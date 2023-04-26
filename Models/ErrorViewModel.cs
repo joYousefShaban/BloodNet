13d4c7c48c3ee29956cbcac7326f88bc686772c2
JoYousefShaban <yousefsha3bola2001@gmail.com>
2023-04-26 20:02:35 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BloodNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
